1a97b4a269b5653ddd50d1b55ec7d78939caacee
MarPeMann <mpetma@hotmail.com>
2015-10-27 15:10:32 +0200

    jquerya

--- controllers/scripts.js
+++ controllers/scripts.js
@@ -1,1 +1,25 @@
 console.log("Toimii...");
+
+$(document).ready(function(){
+    console.log("jquery onload trigger");
+    $("nav").css("background-color","lightblue")
+    .css("padding","20px").css("border-radius","8px");
+    
+    $(".about").html("<b>Jotain muuta</b>");
+    
+    $("[data-dummy]").html("<p>Hello world</p>");
+
+
+});
+
+
+
+/*window.onload = function(event){
+
+console.log(event);
+    
+jokuid.innerHTML = "niin on";
+jokuid.style.backgroundColor = "black";
+}*/
+
+
