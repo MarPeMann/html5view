6971dd0193137f118a86b7eb1bc489ab52cf5fd0
MarPeMann <mpetma@hotmail.com>
2015-10-28 11:32:26 +0200

    ajax functions added

--- controllers/scripts.js
+++ controllers/scripts.js
@@ -8,6 +8,27 @@ $(document).ready(function(){
     $(".about").html("<b>Jotain muuta</b>");
     
     $("[data-dummy]").html("<p>Hello world</p>");
+    
+    var setting = {
+        method:"GET",
+        url:"http://localhost:28017/oma/person/",
+        dataType:"jsonp",
+        jsonp:"jsonp"
+    }
+    
+    $.ajax(setting).done(function(data){
+        console.log(data);
+        for(i = 0; i < data.rows.length; i++){
+            
+            var html = "<tr>" +
+                        "<td>" + data.rows[i].name + "</td>" +
+                        "<td>" + data.rows[i].address + "</td>" +
+                        "<td>" + data.rows[i].age + "</td>" +
+                        "</tr>";
+            
+            $(html).appendTo("tbody");
+        }
+    });
 
 
 });
